91067e5a3b69441fb75ec1c83e22046f4b47a916
Willian Emanuel Corban Silva <2301250@SALAS.AULAS>
2024-10-23 21:18:56 -0300

    segundo commit

--- src/components/ListarUsuarios.js
+++ src/components/ListarUsuarios.js
@@ -1,24 +1,57 @@
-import {useState} from 'react';
-import './CadastroUser.css';
-import { json } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import './ListarUsuarios.css';
 
 function ListarUsuarios() {
+    const [dados, setDados] = useState([]);
 
-    async function puxar_lista() {
+    async function puxarLista() {
         const api = await fetch('http://localhost:8000/api/users');
 
-        let response = api.json();
-        if(api.ok) {
+        if (api.ok) {
+            let response = await api.json();
             console.log(response);
-            return;
+            setDados(response);
+        } else {
+            console.log('Algo deu errado!!');
         }
     }
 
-    return(
+    useEffect(() => {
+        puxarLista();
+    }, []);
+
+    function updateUsuario(id) {
+        alert(id);
+    }
+
+    return (
         <div id="formulario">
-            <button type='button' onClick={puxar_lista}>Puxar Lista</button>
+            <h1>Listagem de Usuários</h1>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>CPF / CNPJ</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dados.map((usuario) => (
+                        <tr key={usuario.id}>
+                            <td>{usuario.id}</td>
+                            <td>{usuario.name}</td>
+                            <td>{usuario.cpf_cnpj}</td>
+                            <td>
+                                <button onClick={() => updateUsuario(usuario.id)}>Atualizar</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
 export default ListarUsuarios;
